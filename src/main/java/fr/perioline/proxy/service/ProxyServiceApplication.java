fb95f71ee364541e1835f5482580d2343bf53c84
youssef smimaa <youssef.smimaa@gmail.com>
2019-02-13 10:55:04 +0100

    kick off

---
+++ src/main/java/fr/perioline/proxy/service/ProxyServiceApplication.java
@@ -0,0 +1,18 @@
+package fr.perioline.proxy.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+
+}
+
