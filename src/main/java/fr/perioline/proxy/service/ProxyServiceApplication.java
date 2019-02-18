59845906217d4cb464d7ddc857f2f016b053bcef
youssef smimaa <youssef.smimaa@gmail.com>
2019-02-18 12:07:30 +0100

    change config properties

--- src/main/java/fr/perioline/proxy/service/ProxyServiceApplication.java
+++ src/main/java/fr/perioline/proxy/service/ProxyServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProxyServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyServiceApplication.class, args);
 	}
